e43bfcd9b4e7bcfc889d39a219fbf1935168f2df
sehee <seeeh0128@gmail.com>
2022-10-31 22:59:08 +0900

    git연결 prac

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Scaffold(
+        appBar: AppBar(
+          title: Text('앱임')
+        ),
+        body: Align(
+          alignment: Alignment.bottomLeft,
+          child: Container(
+            width: double.infinity,
+            height: 50,
+            color: Colors.blue,
+
+          ),
+        )
+
+        ),
+      );
+
+  }
+}
